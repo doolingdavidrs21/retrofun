bafb7217b95ea75fbbe12fa91da06c72fb660478
doolingdavidrs21 <david@rs21.io>
2026-01-23 14:32:18 -0700

    updated chapter1/db.py

--- chapter1/db.py
+++ chapter1/db.py
@@ -1,6 +1,18 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Model(DeclarativeBase):
+    pass
+
+
 
 load_dotenv()
 
-print('Database URL:', os.environ['DATABASE_URL'])
+# print('Database URL:', os.environ['DATABASE_URL'])
+
+engine = create_engine(os.environ["DATABASE_URL"])
+
+
