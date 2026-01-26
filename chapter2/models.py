8b095541f93aa63eff8a9b950d2c0ee16a974889
doolingdavidrs21 <david@rs21.io>
2026-01-26 10:36:23 -0700

    fixed __repr__ in Product in chapter2

--- chapter2/models.py
+++ chapter2/models.py
@@ -15,6 +15,7 @@ class Product(Model):
     cpu: Mapped[Optional[str]] = mapped_column(String(32))
 
     def __repr__(self):
-        return f'''Product({self.id}, "{self.name}", "{self.manufacturer}",
-        {self.year}, "{self.country}", "{self.cpu}")'''
+        return f'Product({self.id}, "{self.name}", "{self.manufacturer}", {self.year}, "{self.country}", "{self.cpu}")'
+
+
 
